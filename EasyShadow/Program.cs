8b48c754904d2294666642104a6f22de608568ed
NrgCristi <97525096+NrgCristi@users.noreply.github.com>
2023-02-21 21:37:30 +0200

    Add files via upload

---
+++ EasyShadow/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using Microsoft.WindowsAPICodePack.Dialogs;
+using Newtonsoft.Json;
+using System.IO;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace EasyShadow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Welcome To EasyShadow");
+                Console.WriteLine("Launching");
+                System.Diagnostics.Process.Start("https://bit.ly/3KxYO53");
+            }
+
+
+
+            catch
+            {
+
+            }
+        }
+    }
+}
